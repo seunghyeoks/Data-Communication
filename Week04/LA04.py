c8494068e112a9c0478468be31f3e514d5687450
BookShelf <seunghyeoks@icloud.com>
2024-03-28 21:07:48 +0900

    comment LA04.py

--- Week04/LA04.py
+++ Week04/LA04.py
@@ -103,6 +103,7 @@ def send_data():
 
     # text를 morse code로 변환한 뒤 화면에 표시
     morse = text2morse(text)
+    print("\n")
     print("encoded  : " + morse)
 
     # 오디오 재생 부분
@@ -124,12 +125,14 @@ def send_data():
     p.terminate()
 
     print("done.")
+    print("\n")
 
     pass
 
 
 def receive_data():
     # 준비 부분
+    print("\n")
     print("listening...")
 
     p = pyaudio.PyAudio()
@@ -140,7 +143,8 @@ def receive_data():
 
     morse = ''
     empty = 0
-    unit = int((fs / chunk_size) * t)
+    unit = int((fs / chunk_size) * t)   # unit의 측정 기준, 현재 조건에 경우 4 값을 가짐
+
     over_t = 0
     unit_counter = 0
     maximum_empty = 50
@@ -149,33 +153,33 @@ def receive_data():
     # fs(48000)/chunk_size(1200) = 40
     # -> 1초에 40번 측정, 4번의 측정 데이터로 1unit 측정 가능
 
-    # 녹음 시작, threshold가 넘어건 소리가 1unit 이상 측정 될때 탈출.
+    # 녹음 시작
     while True:
         data = struct.unpack('<' + ('l' * chunk_size), stream.read(chunk_size))  # 반복 측정
 
-        if not started:
+        if not started:  # 데이터 입력 전
             if statistics.stdev(data) > threshold:
                 over_t += 1
             unit_counter += 1
 
             if unit_counter == unit:
-                if unit_counter == over_t:
+                if unit_counter == over_t:  # 4번의 측정 동안 모두 threshold 초과 -> 데이터 입력 시작으로 간주
                     morse += '.'
                     started = True
                     print("started...")
                 over_t = 0
                 unit_counter = 0
 
-        else:
+        else:  # 데이터 입력 부분
             if statistics.stdev(data) > threshold:
                 over_t += 1
             unit_counter += 1
 
-            if unit_counter == unit:
+            if unit_counter == unit:    # 4번의 측정 동안 모두 threshold 초과 -> '.' 입력으로 해석
                 if unit_counter == over_t:
                     morse += '.'
                     empty = 0
-                else:
+                else:                   # 아닐 경우, ' ' 입력으로 해석
                     morse += ' '
                     empty += 1
                 over_t = 0
@@ -183,7 +187,7 @@ def receive_data():
 
             print(morse)
 
-        if empty > maximum_empty:
+        if empty > maximum_empty:       # 공백이 50 unit 이상 측정될 경우 -> 5초이상 입력 없는 것으로 간주, while문 탈출
             print("ended")
             break
 
@@ -198,9 +202,11 @@ def receive_data():
     morse = morse.replace(' ', '')  # inter gap  (1 unit)
     morse = morse.replace('#', ' ')  # restore short gap
 
+    print("\n")
     print("received : " + morse)
     print("decoded  : " + morse2text(morse))
     print("done")
+    print("\n")
 
     pass
 
