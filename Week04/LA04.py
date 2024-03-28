50924983e35ab432e93ace670ecc2ae1c1aabdae
BookShelf <seunghyeoks@icloud.com>
2024-03-28 19:21:02 +0900

    edit receive

--- Week04/LA04.py
+++ Week04/LA04.py
@@ -140,8 +140,11 @@ def receive_data():
 
     morse = ''
     empty = 0
-    temp = 0
-    in_counter = 0
+    unit = int((fs / chunk_size) * t)
+    over_t = 0
+    unit_counter = 0
+    maximum_empty = 50
+    started = False
 
     # fs(48000)/chunk_size(1200) = 40
     # -> 1초에 40번 측정, 4번의 측정 데이터로 1unit 측정 가능
@@ -149,43 +152,46 @@ def receive_data():
     # 녹음 시작, threshold가 넘어건 소리가 1unit 이상 측정 될때 탈출.
     while True:
         data = struct.unpack('<' + ('l' * chunk_size), stream.read(chunk_size))  # 반복 측정
-        if statistics.stdev(data) > threshold:
-            for _ in range(0, math.ceil((fs / chunk_size) * t) - 1):  # 현재 변수 조건에선 3번 반복됨
-                data = struct.unpack('<' + ('l' * chunk_size), stream.read(chunk_size))
-                if statistics.stdev(data) > threshold:
-                    temp += 1
 
-            if temp > 3:  # threshold를 넘어선 소리가 4번 이어질 경우, 1unit '.'으로 판단
-                morse += '.'
-                break
-            else:         # 아닐 경우, 잡음으로 판단하고 계속 대기
-                continue
-
-    # 측정 부분
-    while True:
-        for _ in range(0, math.ceil((fs / chunk_size) * t)):  # 현재 변수 조건에선 4번 반복됨.
-            data = struct.unpack('<' + ('l' * chunk_size), stream.read(chunk_size))
+        if not started:
             if statistics.stdev(data) > threshold:
-                temp += 1
+                over_t += 1
+            unit_counter += 1
 
-        if temp > 3:    # 4번의 측정에서 전부 threshold를 넘겼을 경우, 유효한 신호로 판단
-            morse += '.'
-            empty = 0
-        else:           # 아닐 경우, 공백
-            morse += ' '
-            empty += 1
+            if unit_counter == unit:
+                if unit_counter == over_t:
+                    morse += '.'
+                    started = True
+                    print("started...")
+                over_t = 0
+                unit_counter = 0
 
-        print(morse)
-        temp = 0
+        else:
+            if statistics.stdev(data) > threshold:
+                over_t += 1
+            unit_counter += 1
 
-        if empty > 50:  # 공백이 50이상일 경우 -> 5초이상 무음으로 판단, 녹음 종료
+            if unit_counter == unit:
+                if unit_counter == over_t:
+                    morse += '.'
+                    empty = 0
+                else:
+                    morse += ' '
+                    empty += 1
+                over_t = 0
+                unit_counter = 0
+
+            print(morse)
+
+        if empty > maximum_empty:
+            print("ended")
             break
 
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    morse = morse.strip()   # 양쪽 여백 제거
+    morse = morse.strip()  # 양쪽 여백 제거
     morse = morse.replace('...', '-')  # 3 straights to dash
     morse = morse.replace('       ', '#$#')  # temp medium gap (7 units)
     morse = morse.replace('   ', '#')  # temp short gap  (3 units)
