259f0001076d1fa717fd9e2f65284fd603189df5
BookShelf <seunghyeoks@icloud.com>
2024-04-09 13:07:58 +0900

    fix: Changed practice code rules for MBP14" [Week06]

--- DC_Week6/pr4.py
+++ DC_Week6/pr4.py
@@ -19,24 +19,24 @@ def audio2file(audio_sample, filename):
             w.writeframes(struct.pack('<l', a))
 
 
-rules = {'START': 512,
-         '0': 768,
-         '1': 896,
-         '2': 1024,
-         '3': 1152,
-         '4': 1280,
-         '5': 1408,
-         '6': 1536,
-         '7': 1664,
-         '8': 1792,
-         '9': 1920,
-         'A': 2048,
-         'B': 2176,
-         'C': 2304,
-         'D': 2432,
-         'E': 2560,
-         'F': 2688,
-         'END': 2944}
+rules = {'START': 520,
+         '0': 760,
+         '1': 880,
+         '2': 1000,
+         '3': 1120,
+         '4': 1240,
+         '5': 1360,
+         '6': 1480,
+         '7': 1600,
+         '8': 1720,
+         '9': 1840,
+         'A': 1960,
+         'B': 2080,
+         'C': 2200,
+         'D': 2320,
+         'E': 2440,
+         'F': 2560,
+         'END': 2800}
 
 text = '🧡💛💚💙💜'
 string_hex = text.encode('utf-8').hex().upper()
@@ -57,7 +57,7 @@ stream = p.open(format=pyaudio.paInt32,
                 rate=samplerate,
                 output=True)
 
-chunk_size = 1024
+chunk_size = 1200
 for i in range(0, len(audio), chunk_size):
     chunk = audio[i:i + chunk_size]
     stream.write(struct.pack('<' + ('l' * len(chunk)), *chunk))
